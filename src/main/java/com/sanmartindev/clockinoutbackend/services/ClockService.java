cbe33e0e8bc0a75c5cbc921102dfb10a00d0f5ad
= <lipesanmartin@hotmail.com>
2023-10-14 15:34:17 -0300

    POST and PUT receive String parses to LocalTime

--- src/main/java/com/sanmartindev/clockinoutbackend/services/ClockService.java
+++ src/main/java/com/sanmartindev/clockinoutbackend/services/ClockService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.time.LocalTime;
 import java.util.List;
 
 @Service

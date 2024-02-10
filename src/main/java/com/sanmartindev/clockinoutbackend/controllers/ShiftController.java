fd4dcaebbeceb3beeebcc507fffe807e64dd1244
Felipe Sanmartin <lipesanmartin@hotmail.com>
2024-02-10 14:27:03 -0300

    endpoint path changed

--- src/main/java/com/sanmartindev/clockinoutbackend/controllers/ShiftController.java
+++ src/main/java/com/sanmartindev/clockinoutbackend/controllers/ShiftController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/api/clocks")
+@RequestMapping(value = "/api/shifts")
 @Tag(name = "Shifts", description = "Shifts API")
 public class ShiftController {
 
